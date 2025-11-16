584f3e6cf3fbe00ee9b91532b8f5e85a4c6349ba
rafag <rgutmed873@iesgrancapitan.org>
2025-11-16 20:51:03 +0100

    ejercicio6 beginner hecho

--- src/Arrays/Beginner/ejercicio6.java
+++ src/Arrays/Beginner/ejercicio6.java
@@ -1,7 +1,45 @@
 package Arrays.Beginner;
 
+import java.util.Random;
+import java.util.Scanner;
+
 public class ejercicio6 {
     static void main(String[] args) {
+        Scanner sc = new Scanner (System.in);
 
+        int [] nElementos = new int [100];
+
+        inicializarArray(nElementos);
+        sumaElementos(nElementos);
+
+    }
+
+    private static void sumaElementos(int[] nElementos) {
+
+        int sumaIndicePar = 0;
+
+        for (int i = 0; i <nElementos.length; i++) {
+
+            if (i %2 == 0) {
+                System.out.println(i + " es par");
+                sumaIndicePar += i;
+            }
+
+        }
+        System.out.println("La suma es: " + sumaIndicePar);
+    }
+
+    private static void inicializarArray(int[] nElementos) {
+        for (int i = 0; i < nElementos.length; i++) {
+            nElementos[i] = (int)pedirNumeroAleatorio(0,100);
+        }
+
+    }
+
+
+    public static int pedirNumeroAleatorio(int min, int max) {
+
+        Random rand = new Random();
+        return rand.nextInt(max-min +1) + min;
     }
 }
